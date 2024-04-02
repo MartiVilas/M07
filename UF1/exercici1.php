e2178cffef7c187a10e5f7c79b3c70a1b858a7bf
MartiVilas <marti.vilas14@gmail.com>
2024-04-02 09:47:46 +0200

    create project

---
+++ UF1/exercici1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!------------------CSS------------------>
+    <link rel="stylesheet" href="./style.css">
+    <!----------------SCRIPT-------------->
+    <script src="./main.js" defer></script>
+
+    <title>Ex1: Mostra numeros del 1 al 100</title>
+</head>
+<!------Mostra els números del 0 al 100 fent servir un bucle for------>
+<body>
+    <?php
+        for($i = 1; $i = 100; $i++){
+            echo $i
+        }
+    ?>
+
+</body>
+</html>
